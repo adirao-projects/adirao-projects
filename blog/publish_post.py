f3e7009c47c5e327bd1841574a9a12ebd5d008b4
Adi Rao <adityakrao.projects@gmail.com>
2024-08-11 01:27:54 -0400

    updated latex compiler

--- blog/publish_post.py
+++ blog/publish_post.py
@@ -20,6 +20,10 @@ parser.add_argument('-p', "--publish", type=int,
                         Otherwise will just add an update tag to a file", 
                     default=0, choices=[0,1])
 
+parser.add_argument('-e', "--engine", default='latexml', 
+                    help="Engine used for conversion (latexml:lml, pandoc:pdc)", 
+                    choices=["lml", "pdc", "htl", "m4t"])
+
 args = parser.parse_args()
 
 cdir = os.getcwd() + '/' + args.name
@@ -27,22 +31,46 @@ fmt = args.format
 date = args.date
 pubish = bool(args.publish)
 
-
 if fmt == "tex":
     fmt_full = "latex"
-    print(f'latexmlc ./{args.name}/{args.name}.{fmt} --dest="./{args.name}/{args.name}.html"')
-    os.system(f'latexmlc ./{args.name}/{args.name}.{fmt} --dest="./{args.name}/{args.name}.html"')
-    input()
 elif fmt == "md":
-    fmt_full = "markdown"
+    fmt_full = "markdown"    
 
-try: 
-	os.system(f"pandoc {cdir}/{args.name}.{fmt} -f {fmt_full} -t html -s -o {cdir}/{args.name}.html")
-	print("Success")
- 
-except Exception as e:
-    print("Failure")
-    print(e)
-
+if args.engine == "lml":
+    if fmt == "tex":
+        try: 
+            os.system(f'latexmlc ./{args.name}/{args.name}.{fmt} --dest="./{args.name}/{args.name}.html"')
+            print("Success")
+            
+        except Exception as e:
+            print("Failure", e)
+            
+    elif fmt == "md":
+        fmt_full = "markdown"
+        
+elif args.engine == "pdc":
+    try: 
+        os.system(f"pandoc {cdir}/{args.name}.{fmt} -f {fmt_full} -t html -s -o {cdir}/{args.name}.html")
+        print("Success")
+    
+    except Exception as e:
+        print("Failure", e)
+        
+elif args.engine == "htl":
+    try: 
+        os.system(f"htlatex {cdir}/{args.name}.{fmt}")
+        print("Success")
+    
+    except Exception as e:
+        print("Failure", e)
+        
+elif args.engine == "m4t":
+    try:
+        os.system(f'make4ht {cdir}/{args.name}.{fmt} "mathml"')
+        print("Success")
+    
+    except Exception as e:
+        print("Failure", e)
+        
 print('Press enter to close')
 input()
